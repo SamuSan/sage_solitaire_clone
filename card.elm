db4a32b09c31a0e9a4425077f6a9cc0609aa73af
Sam Minns <sam.minns@powershop.co.nz>
2017-04-21 13:43:40 +1200

    Add card and svg files for cards

--- card.elm
+++ card.elm
@@ -1,33 +1,61 @@
+module Main exposing (..)
+
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
 
 main =
-  Html.program { model = model, update = update, view = view }
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
 
+
 type alias Model =
-  { value : Int
-  , suit : String
-  , selected : Bool
-  }
+    { value : Int
+    , suit : String
+    , selected : Bool
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model 2 "H" False, Cmd.none )
+
 
 
 -- UPDATE
 
-type Msg =
-  Select
 
-update : Msg -> Model -> Model
+type Msg
+    = Select
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Select ->
-      selected = True
+    case msg of
+        Select ->
+            ( { model | selected = True }, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-  [
-  ]
+    div []
+        [ div [] [ img [ src (String.concat [ "cards/", (String.concat [ (toString model.value), model.suit ]), ".svg" ]), width 50, height 100 ] [] ]
+        ]
